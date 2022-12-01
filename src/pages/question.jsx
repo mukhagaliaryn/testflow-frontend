fcf744d92f578c99307491ab2359340467a619ac
mukhagaliaryn <mukhagaliaryn@gmail.com>
2022-12-02 01:12:45 +0600

    Done

--- src/pages/question.jsx
+++ src/pages/question.jsx
@@ -1,21 +1,21 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import MainLayout from "../layouts/main";
+import React from "react";
+import { useSelector } from "react-redux";
+import MainLayout from "../layouts/main";
 
 
-// const AddQuestion = () => {
-//     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+const AddQuestion = () => {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
-//     return (
-//         <MainLayout
-//             title={"Добавление вопросов"}
-//             heading={"Добавить вопрос"}
-//         >
-//             <div className="main-container">
-//                 <h1>Hello world</h1>
-//             </div>
-//         </MainLayout>
-//     )
-// }
+    return (
+        <MainLayout
+            title={"Добавление вопросов"}
+            heading={"Добавить вопрос"}
+        >
+            <div className="main-container">
+                <h1>Hello world</h1>
+            </div>
+        </MainLayout>
+    )
+}
 
-// export default AddQuestion;
+export default AddQuestion;
