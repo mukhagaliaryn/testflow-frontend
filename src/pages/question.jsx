25f3ff81b3149845d9ef2c46fe3b9593f4e88d44
mukhagaliaryn <mukhagaliaryn@gmail.com>
2022-12-02 00:54:29 +0600

    Done

--- src/pages/question.jsx
+++ src/pages/question.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import MainLayout from "../layouts/main";
-import ReactRichEditor from 'react-rich-text-editor'
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import MainLayout from "../layouts/main";
 
 
-const AddQuestion = () => {
-    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+// const AddQuestion = () => {
+//     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
-    return (
-        <MainLayout
-            title={"Добавление вопросов"}
-            heading={"Добавить вопрос"}
-        >
-            <div className="main-container">
-                {isAuthenticated &&
-                    <ReactRichEditor height={200} />
-                }
-            </div>
-        </MainLayout>
-    )
-}
+//     return (
+//         <MainLayout
+//             title={"Добавление вопросов"}
+//             heading={"Добавить вопрос"}
+//         >
+//             <div className="main-container">
+//                 <h1>Hello world</h1>
+//             </div>
+//         </MainLayout>
+//     )
+// }
 
-export default AddQuestion;
+// export default AddQuestion;
